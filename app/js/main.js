b812a0fb04fcbda7193cf8410481781831dfb234
Just1k01 <ljustfunkl@inbox.ru>
2020-04-19 19:20:55 +0300

    add section slider and fix others

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $(".js-stars").rateYo({
+      rating: 4.5,
+      starWidth: "17px",
+      ratedFill: "#ffc000",
+      readOnly: true
+    });
+
+    $('.slider__wrapper').slick({
+    
+    });
+   
+  });
